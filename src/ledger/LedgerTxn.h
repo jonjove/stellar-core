d8017e3b5c05afc8c52315a42ca97f354d7f7219
Siddharth Suresh <siddharth@stellar.org>
2021-06-24 16:48:59 -0700

    Add LedgerTxnLiquidityPoolSQL and related data layer changes

--- src/ledger/LedgerTxn.h
+++ src/ledger/LedgerTxn.h
@@ -456,6 +456,10 @@ class AbstractLedgerTxnParent
     // anything other than a (real or stub) root LedgerTxn.
     virtual void dropClaimableBalances() = 0;
 
+    // Delete all liquidity pool ledger entries. Will throw when called on
+    // anything other than a (real or stub) root LedgerTxn.
+    virtual void dropLiquidityPools() = 0;
+
     // Return the current cache hit rate for prefetched ledger entries, as a
     // fraction from 0.0 to 1.0. Will throw when called on anything other than a
     // (real or stub) root LedgerTxn.
@@ -721,6 +725,7 @@ class LedgerTxn final : public AbstractLedgerTxn
     void dropOffers() override;
     void dropTrustLines() override;
     void dropClaimableBalances() override;
+    void dropLiquidityPools() override;
     double getPrefetchHitRate() const override;
     uint32_t prefetch(UnorderedSet<LedgerKey> const& keys) override;
 
@@ -777,6 +782,7 @@ class LedgerTxnRoot : public AbstractLedgerTxnParent
     void dropOffers() override;
     void dropTrustLines() override;
     void dropClaimableBalances() override;
+    void dropLiquidityPools() override;
 
 #ifdef BUILD_TESTS
     void resetForFuzzer() override;
